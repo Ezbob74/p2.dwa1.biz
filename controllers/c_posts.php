1efa16a54cd7388e74c1f86476cd9fc09a3f08b6
Babak Mansouri <mansouri@g.harvard.edu>
2013-10-31 21:19:13 -0400

    Exclude the user from following herself add css

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -51,7 +51,9 @@ class posts_controller extends base_controller {
             ON posts.user_id = users_users.user_id_followed
         INNER JOIN users 
             ON posts.user_id = users.user_id
-        WHERE users_users.user_id = '.$this->user->user_id;
+        WHERE users_users.user_id = '.$this->user->user_id.'
+         AND posts.user_id  !='.$this->user->user_id ;
+        //echo $q;
 
     # Run the query, store the results in the variable $posts
     $posts = DB::instance(DB_NAME)->select_rows($q);
@@ -75,7 +77,8 @@ class posts_controller extends base_controller {
 
     # Build the query to get all the users
     $q = "SELECT *
-        FROM users";
+        FROM users 
+        WHERE user_id!=".$this->user->user_id;
 
     # Execute the query to get all the users. 
     # Store the result array in the variable $users
