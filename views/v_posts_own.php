74b748db834d83ff51d5d318022b1039fa13cf6b
Babak Mansouri <mansouri@g.harvard.edu>
2013-11-05 23:40:02 -0500

    if user doesnt have posts tell him so

--- views/v_posts_own.php
+++ views/v_posts_own.php
@@ -1,6 +1,10 @@
+ <?php if($posts): ?>
  <!-- loop through users posts and let her delete them  -->
  <?php foreach($posts as $post): ?>
 
 	<div id='posts'><?=$post['content']?><a href='/posts/delete/<?=$post['post_id']?>'><BR>Delete Post</a></div><br>
 
 <?php endforeach; ?>
+<?php else: ?>
+	You do not have any posts yet.
+<?php endif; ?>
